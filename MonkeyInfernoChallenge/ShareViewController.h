15c49ac30a4cc636532889b507286de58a9c6ab5
Neil Dwyer <neildwyer@Neils-Mac-mini.local>
2014-08-15 14:41:46 -0700

    Removed Facebook and Twitter so it can be built

---
+++ MonkeyInfernoChallenge/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  MonkeyInfernoChallenge
+//
+//  Created by Neil Dwyer on 8/15/14.
+//  Copyright (c) 2014 Neil Dwyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareViewController : UIViewController
+
+@end
